39c4d53a26d4052f672f0542aa878a30480ad7e1
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-19 20:46:58 -0500

    add average age of adoptable pets in admin shelters show page

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe 'Admin Shelters Show Page' do
     expect(page).to have_content('Aurora shelter')
     expect(page).to have_content('Aurora, CO')
   end
+
+  it 'shows average age of all adoptable pets for the shelter' do 
+    shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    shelter_1.pets.create!(name: 'Woof', breed: 'dog', age: 12, adoptable: true)
+    shelter_1.pets.create!(name: 'Ozzy', breed: 'chow', age: 1, adoptable: false)
+
+    visit "/admin/shelters/#{shelter_1.id}"
+    save_and_open_page
+
+    expect(page).to have_content("Shelter Statistics")
+    expect(page).to have_content("Average age of adoptable pets: 6.67")
+  end
 end
